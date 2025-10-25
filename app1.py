a861f2fc52b39b9e5cc7f2557a9b802697d0666c
TharushaGonawala <157585076+TharushaJM@users.noreply.github.com>
2025-10-25 13:09:32 +0530

    Adding Whileloop

--- app1.py
+++ app1.py
@@ -9,13 +9,22 @@ OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-response= client.chat.completions.create(
-    model='gpt-4o' ,
-    messages=[{"role": "user", "content": "Hello!"}],
-    max_tokens=50,
-    n=1,
-    temperature=0
-    
-)
-for choice in response.choices:
-    print(choice.message.content)
+
+while True:
+        
+    question=input("User: ")
+    if question != "Bye":
+        
+        response= client.chat.completions.create(
+            model='gpt-4o' ,
+            messages=[{"role": "user", "content": question}],
+            max_tokens=50,
+            n=1,      
+            temperature=0  #response randomness 
+            
+        )
+        for choice in response.choices:
+            print(f"AI: {choice.message.content}")
+    else:
+        print('AI: Bye....')
+        break
